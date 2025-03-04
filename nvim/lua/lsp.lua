c2140b9818e8ad3a28061210c3788b072242db1a
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2025-03-04 20:27:27 -0300

    changes

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -4,7 +4,7 @@ local lspcontainers = require("lspcontainers")
 -- Configure Python LSP (pylsp)
 lspconfig.pylsp.setup({
   -- Use the Dockerized LSP command
-  cmd = lspcontainers.command("pylsp"),
+  cmd = lspcontainers.command("pyright"),
   -- Ensure the LSP can find your project files
   root_dir = lspconfig.util.root_pattern(".git", "requirements.txt"),
   -- Other LSP settings (e.g., keymaps, capabilities)
