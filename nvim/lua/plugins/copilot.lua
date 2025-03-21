8d6f317e733996e0ad5eeddf6bdb7bd6e279f2e6
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2025-03-21 09:53:40 -0300

    feat: pretty print lf

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -38,7 +38,7 @@ return {
     opts = {
       suggestion = {
         -- enabled = not vim.g.ai_cmp,
-        enabled = false,
+        enabled = true,
         auto_trigger = true,
         hide_during_completion = vim.g.ai_cmp,
         keymap = {
