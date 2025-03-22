7746664bfde8aae58310d224745be76ea9e94c89
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2025-03-22 01:12:29 -0300

    feat: lf

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -58,8 +58,8 @@ end, { desc = "Toggle copilot" })
 --                                             |___/                |_|
 vim.keymap.del("n", "<leader>l")
 
-vim.keymap.set("n", "<C-j>", "<C-d>", { desc = "Hald page down" })
-vim.keymap.set("n", "<C-k>", "<C-u>", { desc = "Half page up" })
+vim.keymap.set("n", "<C-S-J>", "<C-d>", { desc = "Hald page down" })
+vim.keymap.set("n", "<C-S-K>", "<C-u>", { desc = "Half page up" })
 
 vim.keymap.set("n", "ç", ":")
 -- vim.keymap.set("n", ":", "<Nop>")
