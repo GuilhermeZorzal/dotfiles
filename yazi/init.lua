7efdfab91198a35a7f5ba46df1ed1405013ec923
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2025-07-06 21:18:03 -0300

    yazi: githeader

--- yazi/init.lua
+++ yazi/init.lua
@@ -4,6 +4,37 @@ require("git"):setup()
 
 require("no-status"):setup()
 
+require("githead"):setup({
+	-- branch_prefix = "on",
+	branch_symbol = " ",
+	branch_color = "bright black",
+	branch_borders = "()",
+
+	remote_branch_color = "bright magenta",
+	tag_color = "magenta",
+	commit_color = "bright magenta",
+
+	-- symbols ⇣ and ⇡
+	behind_remote_color = "red",
+	ahead_remote_color = "cyan",
+
+	-- symbol $
+	stashes_color = "bright magenta",
+
+	-- symbol ~
+	state_color = "red",
+
+	-- symbol ✔
+	staged_symbol = "✔",
+	staged_color = "green",
+
+	-- symbol !
+	unstaged_color = "bright yellow",
+
+	-- symbol ?
+	untracked_color = "bright blue",
+})
+
 require("relative-motions"):setup({ show_numbers = "relative_absolute", show_motion = true, enter_mode = "first" })
 
 require("bookmarks"):setup({
