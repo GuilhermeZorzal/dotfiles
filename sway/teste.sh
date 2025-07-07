dd8a5f85a437d8d1d113865ce839cbc1aac34993
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2025-07-06 21:17:16 -0300

    sway: fix yazi

--- sway/teste.sh
+++ sway/teste.sh
@@ -1,3 +1,11 @@
 export EDITOR=nvim
 cd ~/Documents/git/
-yazi
+
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd <"$tmp"
+  [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+  rm -f -- "$tmp"
+}
+y
